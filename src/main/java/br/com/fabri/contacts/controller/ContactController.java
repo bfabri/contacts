280a619c5cfa0831300d85ccb8b6e78f4e1eab63
Bruno Fabri <brunoferreirafabri@gmail.com>
2017-05-04 23:06:16 -0300

    #1 - Setting up project using Vraptor4 as web framework

---
+++ src/main/java/br/com/fabri/contacts/controller/ContactController.java
@@ -0,0 +1,13 @@
+package br.com.fabri.contacts.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
+
+@Controller
+public class ContactController {
+
+	@Path("/")
+    public void init() {
+
+    }
+}
